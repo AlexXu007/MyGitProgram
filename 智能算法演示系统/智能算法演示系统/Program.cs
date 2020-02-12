dbcb8da35acdd09c0dd402c87d63bf599aeec2ab
Alex <xu_zhenlin@126.com>
2020-02-12 15:47:09 +0800

    更改

---
+++ 智能算法演示系统/智能算法演示系统/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 智能算法演示系统
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new 智能算法演示系统());
+            Frm_Login login = new Frm_Login();
+
+            //切换登录界面到主程序界面
+            login.ShowDialog();
+            if (login.DialogResult == DialogResult.OK)
+            {
+                login.Dispose();
+                Application.Run(new Frm_MainForm());
+            }
+            else if (login.DialogResult == DialogResult.Cancel)
+            {
+                login.Dispose();
+                return;
+            }
+
+            
+
+
+        }
+    }
+}
